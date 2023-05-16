df23d51c01c331235a56f7465c6076d9aa7c77ff
NIGERIA\oehilen <oehilen@axxess.com>
2023-05-16 12:26:44 +0100

    start-coding

---
+++ src/main/kotlin/Employee.kt
@@ -0,0 +1,12 @@
+class Employee {
+
+    // this class is
+
+    fun calculatePay(money: Double): Double {
+        return money
+    }
+
+    fun save(employee: Employee) : Employee{ // this logic is a persistence logic
+        return employee
+    }
+}
